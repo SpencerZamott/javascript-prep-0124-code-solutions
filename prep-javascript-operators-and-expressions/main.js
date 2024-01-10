6c19959ec08b4426a445845f81335e6248310724
Spencer Zamott <mrzamott@gmail.com>
2024-01-10 04:46:38 +0000

    Completed prep javascript-operators-and-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+let number1 = 5
+let number2 = 8
+let product = number1 * number2
+console.log('value of product:', product)
+console.log('type of variable:', typeof product)
+
+let charge = 9
+let payment = 8
+let amountRemaining = charge - payment
+console.log('value of amount remaining:', amountRemaining)
+console.log('type of amount remaining:', typeof amountRemaining)
+
+let test = 7
+let assignments = 6
+let final = 4
+let grade = test + assignments + final/3
+console.log('Value of grade:', grade)
+console.group('Type of grade:', typeof grade)
+
+let firstName = "Spencer"
+let lastName = "Zamott"
+let fullName = firstName + ' ' + lastName
+console.log('Value of full name:', fullName)
+console.log ('Type of full name', typeof fullName)
+
+const pH = 3
+let isAcidic = pH<7
+console.log('Value of is acidic', isAcidic)
+console.log ('Type of is acidic', typeof isAcidic)
+
+let numberOfSoilders = 100
+let isSparda = numberOfSoilders === 300
+console.log('Value of is Sparda', isSparda)
+console.log('Type of is Sparda', typeof isSparda)
+
+let nickname = fullName + " " + "is the GOAT!"
+console.log('Value of nickname', nickname)
+console.log('Type of nickname', typeof nickname)
