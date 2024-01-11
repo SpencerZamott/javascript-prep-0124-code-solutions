bbb9d7881252b1c5be43c1c20219b20e746b45c9
Spencer Zamott <mrzamott@gmail.com>
2024-01-11 18:45:44 +0000

    Completed javascript operators and expressions

--- prep-javascript-operators-and-expressions/main.js
+++ prep-javascript-operators-and-expressions/main.js
@@ -1,35 +1,35 @@
-let number1 = 5
-let number2 = 8
-let product = number1 * number2
+const number1 = 5
+const number2 = 8
+const product = number1 * number2
 console.log('value of product:', product)
 console.log('type of variable:', typeof product)
 
-let charge = 9
-let payment = 8
-let amountRemaining = charge - payment
+const charge = 9
+const payment = 8
+const amountRemaining = charge - payment
 console.log('value of amount remaining:', amountRemaining)
 console.log('type of amount remaining:', typeof amountRemaining)
 
-let test = 7
-let assignments = 6
-let final = 4
-let grade = test + assignments + final/3
+const test = 7
+const assignments = 6
+const final = 4
+const grade = test + assignments + final/3
 console.log('Value of grade:', grade)
 console.group('Type of grade:', typeof grade)
 
-let firstName = "Spencer"
-let lastName = "Zamott"
-let fullName = firstName + ' ' + lastName
+const firstName = "Spencer"
+const lastName = "Zamott"
+const fullName = firstName + ' ' + lastName
 console.log('Value of full name:', fullName)
 console.log ('Type of full name', typeof fullName)
 
 const pH = 3
-let isAcidic = pH<7
+const isAcidic = pH<7
 console.log('Value of is acidic', isAcidic)
 console.log ('Type of is acidic', typeof isAcidic)
 
-let numberOfSoilders = 100
-let isSparda = numberOfSoilders === 300
+const numberOfSoilders = 100
+const isSparda = numberOfSoilders === 300
 console.log('Value of is Sparda', isSparda)
 console.log('Type of is Sparda', typeof isSparda)
 
